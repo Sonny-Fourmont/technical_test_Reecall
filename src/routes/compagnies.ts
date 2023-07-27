fe177cd184e9f86f3c4d620f2f1f4885c05d5447
Shintaro <sonny.fourmont@epitech.eu>
2023-07-27 14:15:05 +0200

    [ADD] /compagnies GetAll request

--- src/routes/compagnies.ts
+++ src/routes/compagnies.ts
@@ -1,8 +1,18 @@
 import express, {Router, Request, Response} from "express";
+import {ApiCompagnies} from "../api/api";
 const router: Router = express.Router();
 
 router.get('/compagnies', (req: Request, res: Response) => {
-    res.send('compagnies test');
+    async function GetThemAll() {
+        const CompApi = new ApiCompagnies();
+        try {
+            const result = await CompApi.GetCompagnies();
+            res.json(result);
+        } catch (error) {
+            console.error('Error');
+        }
+    }
+    GetThemAll();
 });
 router.get(`/compagnies/:id`, (req: Request, res: Response) => {
     res.send(`id test: ${req.params.id}`);
