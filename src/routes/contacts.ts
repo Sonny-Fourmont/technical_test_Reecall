c4529fdf463f4348bcd56805eaf0f6f2a264f498
Shintaro <sonny.fourmont@epitech.eu>
2023-07-31 15:18:07 +0200

    [ADD] GetAll and GetID for each features

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -1,11 +1,35 @@
 import express, {Router, Request, Response} from "express";
+import { ApiContacts, ApiContactsID } from "../api/api";
 const router: Router = express.Router();
 
 router.get('/contacts', (req: Request, res: Response) => {
-    res.send('contacts test');
+    async function GetThemAll() {
+        const ContactAPI = new ApiContacts();
+        try {
+            const result = await ContactAPI.GetContacts();
+            console.log("Contacts list's Status: 200 OK")
+            res.json(result);
+        } catch (error) {
+            console.error('Error 404: Contacts not found');
+            res.send('Error 404: Contacts not found');
+        }
+    }
+    GetThemAll();
 });
 router.get(`/contacts/:id`, (req: Request, res: Response) => {
-    res.send(`id test: ${req.params.id}`);
+    // res.send(`id test: ${req.params.id}`);
+    async function GetThemID() {
+        const ContactID = new ApiContactsID();
+        try {
+            const result = await ContactID.GetContactsID(req.params.id);
+            console.log("Contact ID's Status: 200 OK")
+            res.json(result);
+        } catch (error) {
+            console.error('Error 404: Contact not found. Contacts Id are usually numeric.');
+            res.send('Error 404: Contact not found. Contacts Id are usually numeric.');
+        }
+    }
+    GetThemID();
 });
 router.post('/contacts/create', (req: Request, res: Response) => {
     res.send("create test");
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,36 @@
 import express, {Router, Request, Response} from "express";
+import { ApiTransactions, ApiTransactionsID } from "../api/api";
 const router: Router = express.Router();
 
 router.get('/transactions', (req: Request, res: Response) => {
-    res.send('transactions test');
+    // res.send('transactions test');
+    async function GetThemAll() {
+        const DealsApi = new ApiTransactions();
+        try {
+            const result = await DealsApi.GetDeals();
+            console.log("Transactions list's Status: 200 OK")
+            res.json(result);
+        } catch (error) {
+            console.error('Error 404: Transactions not found');
+            res.send('Error 404: Transactions not found');
+        }
+    }
+    GetThemAll();
 });
 router.get(`/transactions/:id`, (req: Request, res: Response) => {
-    res.send(`id test: ${req.params.id}`);
+    // res.send(`id test: ${req.params.id}`);
+    async function GetThemID() {
+        const DealsID = new ApiTransactionsID();
+        try {
+            const result = await DealsID.GetDealsID(req.params.id);
+            console.log("Transaction ID's Status: 200 OK")
+            res.json(result);
+        } catch (error) {
+            console.error('Error 404: Transaction not found. Transactions Id are usually numeric.');
+            res.send('Error 404: Transaction not found. Transactions Id are usually numeric.');
+        }
+    }
+    GetThemID();
 });
 router.post('/transactions/create', (req: Request, res: Response) => {
     res.send("create test");
